f43143337727065fdb005510ee966212fa09b860
tanvirtbf <tanver018765@gmail.com>
2024-12-04 10:36:08 +0600

    App.jsx change with some dummy data and table

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const data = [
+    {
+      id: 1, 
+      gender: 'Male',
+      salary: 24000
+    },
+    {
+      id: 2, 
+      gender: 'Female',
+      salary: 12999,
+    },
+    {
+      id: 3, 
+      gender: 'Male',
+      salary: 32000
+    }
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Gender</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Male</td>
+            <td>22000</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Female</td>
+            <td>15000</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Male</td>
+            <td>36000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
