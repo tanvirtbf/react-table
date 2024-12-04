b32498885e40c0d49f366b6d36782699f89cf6a3
tanvirtbf <tanver018765@gmail.com>
2024-12-04 10:59:06 +0600

    basic setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,63 @@
-import { useTable } from 'react-table'
+import { useTable } from "react-table";
 
+const data = [
+  {
+    id: 1,
+    gender: "Male",
+    salary: 24000,
+  },
+  {
+    id: 2,
+    gender: "Female",
+    salary: 12999,
+  },
+  {
+    id: 3,
+    gender: "Male",
+    salary: 32000,
+  },
+];
+
+const columns = [
+  {
+    Header: "ID",
+    accessor: "id",
+  },
+  {
+    Header: "Gender",
+    accessor: "gender",
+  },
+  {
+    Header: "Salary",
+    accessor: "salary",
+  },
+];
 
 function App() {
-  const data = [
-    {
-      id: 1,
-      gender: "Male",
-      salary: 24000,
-    },
-    {
-      id: 2,
-      gender: "Female",
-      salary: 12999,
-    },
-    {
-      id: 3,
-      gender: "Male",
-      salary: 32000,
-    },
-  ];
-
-  const columns = [
-    {
-      Header:"ID",
-      accessor:"id"
-    },
-    {
-      Header:"Gender",
-      accessor:"gender"
-    },
-    {
-      Header:"Salary",
-      accessor:"salary"
-    },
-  ]
-
-  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({
-    columns,
-    data
-  })
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
 
   return (
     <div className="container">
       <table {...getTableProps()}>
         <thead>
-          <tr>
+          {headerGroups.map((hg) => (
+            <tr key={hg} {...hg.getHeaderGroupProps()}>
+              {hg.headers.map((header) => (
+                <th key={header} {...header.getHeaderProps()}>
+                  {header.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+          {/* <tr>
             <th>ID</th>
             <th>Gender</th>
             <th>Salary</th>
-          </tr>
+          </tr> */}
         </thead>
         <tbody>
           {data.map((i) => (
